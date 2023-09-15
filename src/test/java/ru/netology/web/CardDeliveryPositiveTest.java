57f5d8580f82802033021516618d611b9d06d0e5
Nik test <nik.zyryanov.01@gmail.com>
2023-09-15 21:48:52 +0300

    Initial Commit

--- src/test/java/ru/netology/web/CardDeliveryPositiveTest.java
+++ src/test/java/ru/netology/web/CardDeliveryPositiveTest.java
@@ -1,6 +1,7 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -9,29 +10,37 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
 public class CardDeliveryPositiveTest {
-    private String generateDate(int addDays, String pattern) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
-    }
-    @Test
-
-    public void shouldReturnSuccessIfFieldsAreFilledInCorrectly() {
+    @BeforeEach
+    void setUp() {
         open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Уфа");
-        String currentDate = generateDate(4, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").sendKeys(currentDate);
-        $("[data-test-id='name'] input").setValue("Никита Зырянов");
-        $("[data-test-id='phone'] input ").setValue("+79872408117");
-        $("[data-test-id='agreement']").click();
-        $("button.button").click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
     }
+
+    @Test
+    public void shouldReturnSuccessIfFieldsAreFilledInCorrectly() {
+
+        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateText = deliveryDateCard.format(formatter);
+
+        $("[data-test-id=city] [placeholder='Город']").setValue("Уфа");
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(dateText);
+        $("[data-test-id=name] [type=text]").setValue("Никита Зырянов");
+        $("[data-test-id=phone] [type=tel]").setValue("+79872499117");
+        $("[data-test-id=agreement]").click();
+        $("[role=button] .button__content").click();
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
+    }
+
 
 }
